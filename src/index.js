066da89ef24fef064a88a5b371530e75e65ceb48
Omisakin Joshua <officialjoshua9@gmail.com>
2024-01-08 21:29:02 +0100

    "updated index.js"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
-
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
