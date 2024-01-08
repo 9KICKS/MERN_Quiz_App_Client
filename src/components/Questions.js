ebf8f697da6338a563251d563a5c0d0c142fe5a5
Omisakin Joshua <officialjoshua9@gmail.com>
2024-01-08 21:24:59 +0100

    "added Questions.js"

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useFetchQestion } from '../hooks/FetchQuestion';
-import { updateResult } from '../hooks/setResult';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useFetchQuestion } from '../hooks/FetchQuestion'
+import { updateResult } from '../hooks/setResult'
 
 export default function Questions({ onChecked }) {
-    const [checked, setChecked] = useState(undefined);
+    const [checked, setChecked] = useState(undefined)
     const { trace } = useSelector(state => state.questions);
     const result = useSelector(state => state.result.result);
-    const [{ isLoading, apiData, serverError }] = useFetchQestion();
+    const [{ isLoading, apiData, serverError}] = useFetchQuestion();
     const questions = useSelector(state => state.questions.queue[state.questions.trace]);
     const dispatch = useDispatch();
 
@@ -41,7 +41,7 @@ export default function Questions({ onChecked }) {
                             />
 
                             <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-                            <div className={`check ${result[trace] == i ? 'checked' : ''}`}></div>
+                            <div className={`check ${result[trace] === i ? 'checked' : ''}`}></div>
                         </li>
                     ))
                 }
